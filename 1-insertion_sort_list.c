1aba363e16bae01f11a7b3a4fbdacb479005f620
Mitchell <mmoscovics@yahoo.com>
2020-02-02 19:35:04 -0500

    [Bugfix] condition changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,16 +14,18 @@ void insertion_sort_list(listint_t **list)
         return;
     while (current->next != NULL)
     {
-        loop = current;
-        while (loop != NULL)
+        loop = current->next;
+        while (loop->prev != NULL)
         {
-            pos = loop;
-            swap = loop->next;
+            pos = loop->prev;
+            swap = loop;
             if (pos->n > swap->n)
             {
-                swap->next->prev = pos;
+                if (swap->next != NULL)
+                    swap->next->prev = pos;
+                if (pos->prev != NULL)
+                    pos->prev->next = swap;
                 swap->prev = pos->prev;
-                pos->prev->next = swap;
                 pos->prev = swap;
                 pos->next = swap->next;
                 swap->next = pos;

11d116733b95cacacb15cc80f4fc3c456e001a09
mmoscovics <mmoscovics@yahoo.com>
2020-02-03 18:19:49 +0000

    [Bugfix] add head null condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,31 +8,34 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = *list, *loop, *pos, *swap;
+	listint_t *current, *loop, *pos, *swap;
 
-    if (current == NULL || current->next->next == NULL)
-        return;
-    while (current->next != NULL)
-    {
-        loop = current->next;
-        while (loop->prev != NULL)
-        {
-            pos = loop->prev;
-            swap = loop;
-            if (pos->n > swap->n)
-            {
-                if (swap->next != NULL)
-                    swap->next->prev = pos;
-                if (pos->prev != NULL)
-                    pos->prev->next = swap;
-                swap->prev = pos->prev;
-                pos->prev = swap;
-                pos->next = swap->next;
-                swap->next = pos;
-                print_list(*list);
-            }
-            loop = loop->prev;
-        }
-        current = current->next;
-    }
+	if (list == NULL || *list == NULL)
+		return;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		loop = current;
+		while (loop != NULL)
+		{
+			pos = loop;
+			swap = current;
+			if (pos->n > swap->n)
+			{
+				if (swap->next != NULL)
+					swap->next->prev = pos;
+				if (pos->prev != NULL)
+					pos->prev->next = swap;
+				swap->prev = pos->prev;
+				pos->prev = swap;
+				pos->next = swap->next;
+				swap->next = pos;
+				if (swap->prev == NULL)
+					*list = swap;
+				print_list(*list);
+			}
+			loop = loop->prev;
+		}
+		current = current->next;
+	}
 }

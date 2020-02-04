5ec634be500adfa2c98c188a0f8ccbea575edd18
Kathleen McKiernan <851@holbertonschool.com>
2020-02-04 23:02:32 +0000

    debug bubble, fix flags, change in both loops

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,19 +23,26 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t temp1;
 	size_t temp2;
+	int flag;
 
+	if (size < 2)
+		return;
 	if ((array) && (size > 2))
 	{
-		for (temp1 = 0; temp1 < (size - 1); temp1++)
+		for (temp1 = 0; temp1 < size; temp1++)
 		{
-			for (temp2 = 0; temp2 < (size - temp1 - 1); temp2++)
+			flag = 0;
+			for (temp2 = 0; temp2 < (size - 1); temp2++)
 			{
 				if (array[temp2] > array[temp2 + 1])
 				{
 					swap(&array[temp2], &array[temp2 + 1]);
 					print_array(array, size);
+					flag = 1;
 				}
 			}
 		}
+		if (flag == 0)
+			return;
 	}
 }
